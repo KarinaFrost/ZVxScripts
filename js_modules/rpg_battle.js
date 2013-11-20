f3a2715cbff4c55dc4229142010aabdd332e6ed2
ArchZombie0x <archzombielord@gmail.com>
2013-11-20 01:32:17 -0500

    Bugworkaround

--- js_modules/rpg_battle.js
+++ js_modules/rpg_battle.js
@@ -467,7 +467,7 @@
                      var roundnum = battle.round;
 
                      if (roundnum > 100) roundnum = 100 + (rounnum - 100) / 2;
-                     var mult = nn(Math.min(roundnum*10 + nn(battle.xexp), roundnum*15)/tot);
+                     var mult = nn(Math.min(roundnum*10 + 0/*nn(battle.xexp)*/, roundnum*15)/tot);
 
                      for (x in trackr)
                      {
