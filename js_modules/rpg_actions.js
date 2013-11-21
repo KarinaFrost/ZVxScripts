78eccf04a42065ed64e280ca2ec0a9a238c27306
ArchZombie0x <archzombielord@gmail.com>
2013-11-21 10:50:47 -0500

    fix

--- js_modules/rpg_actions.js
+++ js_modules/rpg_actions.js
@@ -191,7 +191,7 @@
                  }
                  else
                  {
-                     this.com.message(src, "Character transfered.");
+                     this.com.broadcast(src, newp.name + "'s RPG character transfered to " + this.user.name(src));
                  }
              }
 
