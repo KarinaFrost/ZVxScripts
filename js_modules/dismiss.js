f103e7d8994f732e81014d314e5b2af0b30fa503
ArchZombie0x <archzombielord@gmail.com>
2013-11-27 14:51:31 -0500

    Add /dismiss

---
+++ js_modules/dismiss.js
@@ -0,0 +1,24 @@
+({
+     require: ["commands"],
+
+     meta:
+     {
+         commands: ["dismiss"]
+     },
+
+     dismiss:
+     {
+         category: "basic",
+         perm: function () {return true;},
+         code:
+         function (src, cmd, chan)
+         {
+
+             this.com.message(src, "Dismissed the announcement.");
+             sys.setAnnouncement("", src);
+         }
+
+
+     }
+
+ });
