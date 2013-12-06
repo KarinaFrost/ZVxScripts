212dfc92c0f7417bfaf8dcea6c62c74562ff05fb
Karina <goldypmu@hotmail.com>
2013-12-06 10:23:59 -0600

    Two new things.
    If this is the base of the crafting system, I'm enjoying what I'm looking at.

--- js_modules/rpg_mixins.js
+++ js_modules/rpg_mixins.js
@@ -3,7 +3,9 @@
      {
          electrum: { output: 2, input: { gold:1, silver: 1}},
          brass: {output:3, input: { copper:2, zinc: 1}},
-         steel: {output:10, input: {iron: 9, graphite: 1}}
+         steel: {output:10, input: {iron: 9, graphite: 1}},
+         wdsteel: {output:5, input: {wdscale: 3. steel: 2}},
+         blkdsteel: {output:5, input: {blkdscale: 3. steel: 2}},
      }
 
 
